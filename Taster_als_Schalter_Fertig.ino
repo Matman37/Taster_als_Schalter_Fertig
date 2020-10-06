4a567864afdeebb5140ffe5e97512a949b99ebf5
Matman37 <71316447+Matman37@users.noreply.github.com>
2020-10-06 13:54:46 +0200

    Add files via upload

---
+++ Taster_als_Schalter_Fertig.ino
@@ -0,0 +1,28 @@
+#include <OneButton.h>
+#define LED 10
+
+OneButton taster(2, false);
+bool merkerLed = false;
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+
+
+  taster.attachClick(Taster_Funktion);
+
+}
+
+void loop()
+{
+  taster.tick();
+
+  delay(10);
+  digitalWrite (LED, merkerLed);
+}
+
+void Taster_Funktion()
+{
+  merkerLed = !merkerLed;
+}
+
